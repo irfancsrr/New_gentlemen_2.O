75529b1e6e3db70861298ecf30ef975d5d84fdbe
irfan <irfancsrrsimt@gmail.com>
2026-08-14 00:35:08 +0530

    minor edit final in backend / api

--- backend/server/utils/generateTokens.js
+++ backend/server/utils/generateTokens.js
@@ -27,7 +27,7 @@ const setAuthCookies = (res, accessToken, refreshToken) => {
     httpOnly: true,
     secure: isProd,
     sameSite: isProd ? "none" : "lax",
-    maxAge: 15 * 60 * 1000,
+    maxAge: 7*24*60 * 60 * 1000,
   });
 
   res.cookie("refreshToken", refreshToken, {
